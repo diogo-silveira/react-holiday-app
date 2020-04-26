23946c3d7105f30e940e57f216564e7f6501c0f1
Diogo <fraga.diogo@gmail.com>
2020-04-26 10:39:56 +1000

    Added state to form

--- src/Holiday/Holiday.js
+++ src/Holiday/Holiday.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Holiday.css';
 
-
 const Title = () => {
     return (
         <div className="row px-5">
@@ -23,74 +22,119 @@ const LabelEngagementQuestion = () => {
     );
 }
 
-const SelectCountry = () => {
-    return (
-        <div className="container row px-5">
-            <div className="col-12 form-group text-left">
-                <span>Select your country</span>
-                <select className="custom-select" size="3">
-                    <option defaultValue>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
+class Holiday extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCountry: 'default',
+            isLongWeekend: true,
+            firstName: ''            
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
-            </div>
-        </div>
-    );
-}
+    handleCountryOnChange = (event) => {
+        console.log(this.state.selectedCountry);
+        console.log("Event" + event);
+        this.setState({ selectedCountry: event.target.value});
+    }
+   
+    handleSubmit = (event) => {
+        console.log(event);
+        event.preventDefault();
+    }
 
-const LongWeekend = () => {
-    return (
-        <div className="container row px-5">
-            <div className="col-3 text-left">
-                <span>Long weekend?</span>
-            </div>
-            <div className="col-9 d-flex">
-                <label className="switch">
-                    <input className="primary" type="checkbox"/>
-                    <span className="slider round"></span>
-                </label>
-            </div>
-        </div>
-    );
-}
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.name === 'isLongWeekend' ? target.checked : target.value;
+        const name = target.name;
+        console.log(name+'='+value);
+        this.setState({
+          [name]: value
+        });
+      }
 
-const FirstName = () => {
-    return (
-        <div className="container row px-5">
-            <div className="col-12 form-group text-left">
-                <span>First name</span>
-                <input className="form-control" type="text"/>
-            </div>
-        </div>
-    );
-}
 
-const SearchButton = () => {
-    return (
-        <div className="container row px-5">
-            <div className="col-12 form-group text-center">
-                <button type="submit" className="btn btn-primary mb-2 px-5">Search</button>
+    SelectCountry = () => {
+        return (
+            <div className="container row px-5">
+                <div className="col-12 form-group text-left">
+                    <span>Select your country</span>
+                    <select name="selectedCountry" value={ this.state.selectedCountry } onChange={ this.handleInputChange } 
+                        className="custom-select" size="3">
+                        <option value="default" defaultValue>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    LongWeekend = () => {
+        return (
+            <div className="container row px-5">
+                <div className="col-3 text-left">
+                    <span>Long weekend?</span>
+                </div>
+                <div className="col-9 d-flex">
+                    <label className="switch">
+                        <input className="primary" 
+                            name="isLongWeekend"
+                            onChange={ this.handleInputChange } 
+                            checked={ this.state.isLongWeekend } type="checkbox"/>
+                        <span className="slider round"></span>
+                    </label>
+                </div>
+            </div>
+        );
+    }
+
+    FirstName = () => {
+        return (
+            <div className="container row px-5">
+                <div className="col-12 form-group text-left">
+                    <span>First name</span>
+                    <input className="form-control" 
+                        name="firstName"
+                        onChange={ this.handleInputChange }
+                        value={ this.state.firstName } type="text"/>
+                </div>
+            </div>
+        );
+    }
+
+    SearchButton = () => {  
+        return (
+            <div className="container row px-5">
+                <div className="col-12 form-group text-center">
+                    <button type="submit" className="btn btn-primary mb-2 px-5">Search</button>
+                </div>
+            </div>
+        );
+    }
+
+
+    render() {
+        return ( 
+            <div className="Holiday bg-light position-absolute round rounded w-100">
+                <div className="position-relative">
+                    <form onSubmit={ this.handleSubmit }>
+                        { Title() }
+                        { LabelEngagementQuestion() }
+                        { this.SelectCountry() }
+                        { this.LongWeekend() }
+                        { this.FirstName() }
+                        { this.SearchButton() }  
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
-const Holiday = () => {
-    return ( 
-        <div className="Holiday bg-light position-absolute round rounded w-100">
-            <div className="position-relative">
-                { Title() }
-                { LabelEngagementQuestion() }
-                { SelectCountry() }
-                { LongWeekend() }
-                { FirstName() }
-                { SearchButton() }
-            </div>
-        </div>
-        
-    );
- }
 
  export default Holiday;
 
